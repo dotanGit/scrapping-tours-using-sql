a5bbf96b0b62a02bf02e9a8e7bc2ebc5176eb851
dotan <frenkeldotan1998@gmail.com>
2023-04-18 15:10:13 +0300

    Sending email with the upcoming event details

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import selectorlib
-
-URL = "https://programmer100.pythonanywhere.com/tours/"
+import smtplib
+import ssl
+import time
 
 def scrape(url):
     """ Scrape the page source from the URL"""
@@ -14,13 +15,42 @@ def extract(source):
     value = extractor.extract(source)["tours"]   # tours the name of text inside the extract.yaml file
     return value
 
+def send_email(message):
+    port = 465   # For ssl
+
+    PASSWORD = "oymabiteixnekwwv"
+    SENDER = "mypythonprotfolio@gmail.com"
+    RECEIVER = "mypythonprotfolio@gmail.com"
+
+    context = ssl.create_default_context()   # Create a secure ssl context
+
+    email_message = f"Subject: New upcoming tour was found\n\n{message}"
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(SENDER, PASSWORD)
+        server.sendmail(SENDER, RECEIVER, email_message)
+    print("Email was sent")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        text = file.read()
+    return text
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
 
 if __name__ == "__main__":
-  scraped = scrape(URL)
-  extracted = extract(scraped)
-  print(extracted)
-
-
-
-
-
+    while True:
+      scraped = scrape(URL)
+      extracted = extract(scraped)
+      print(extracted)
+      content = read(extracted)
+      if extracted != "No upcoming tours":
+          if extracted not in content:
+              store(extracted)
+              send_email(extracted)
+      time.sleep(2)   # every 2 seconds the program checks for a new tour
