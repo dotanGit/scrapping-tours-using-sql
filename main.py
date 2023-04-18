e8a8583ed7e8b98c13f82fa1cd23ebddd151ffc3
dotan <frenkeldotan1998@gmail.com>
2023-04-18 13:11:55 +0300

    Add scarping function to return source code

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import requests
+import selectorlib
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+
+def scrape(url):
+    """ Scrape the page source from the URL"""
+    response = requests.get(url)
+    source = response.text
+    return source
+
+def extract(source):
+    selectorlib.Extractor.from_yaml_file("extract.yaml")
+
+if __name__ == "__main__":
+    print(scrape(URL))
+
+
+
+
 
